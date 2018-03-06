f41b7d5ce0aee07fb9dd0b4842a55b8f3ae0fa6c
Alexander <dodecad@outlook.com>
2018-03-06 18:19:44 +0300

    Unit test for the constructor

---
+++ Tests/WordCounter.Core.Tests/ParallelWordCounterTests/ParallelWordCounterTests_Constructor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCounter.Core.Tests.ParallelWordCounterTests
+{
+    [TestClass]
+    public class ParallelWordCounterTests_Constructor : ParallelWordCounterTests
+    {
+        private IFileSystem _fileSystemArgument;
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_WhenFileSystemArgumentIsNull_ExpectArgumentNullException()
+        {
+            // Arrange:
+            this._fileSystemArgument = null;
+            this.Stub();
+
+            try
+            {
+                // Act:
+                var result = this.Act();
+            }
+            catch (ArgumentNullException exception)
+            {
+                // Assert:
+                Assert.AreEqual("fileSystem", exception.ParamName);
+                this.AssertCore();
+                throw;
+            }
+        }
+
+        [TestInitialize]
+        public override void TestInitialise()
+        {
+            base.TestInitialise();
+
+            this._fileSystemArgument = this.FileSystem;
+        }
+
+        private ParallelWordCounter Act()
+        {
+            return new ParallelWordCounter(this._fileSystemArgument);
+        }
+    }
+}
